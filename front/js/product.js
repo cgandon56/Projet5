25dfeccf10ea6d7029cbd71d9ddaf799572b98ba
cgandon56 <108945404+cgandon56@users.noreply.github.com>
2022-11-12 09:31:23 +0100

    ajout fichier

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,6 @@
 //URLSearchParams.get()
 //Ajout de l'id des produits à l'url
-let params = new URL(document.location).searchParams;
+const params = new URL(document.location).searchParams;
 let idProduct = params.get("id");
 
 
@@ -9,27 +9,31 @@ async function article() {
 fetch(`http://localhost:3000/api/products/${idProduct}`)//adresse URL à aller chercher
 .then(response => response.json())// réponse du résultat en json
 .then((data)=> {
-    console.log(data) ;
-
+    console.log(data);
+    showdata
 
 
 //Sélection des sections et affichage des détails
 
-let image = (document.querySelector(".item__img"));
-console.log(image);
-image.innerHTML=`<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-let title = (document.querySelector("#title"));
-console.log(title);
-title.innerHTML = `${data.name}`;
-let price = (document.querySelector("#price"));
-console.log(price);
-price.innerHTML= `${data.price}`;
-let description = (document.querySelector("#description"));
-console.log(description);
-description.innerHTML=`${data.description}`;
 }
 )};
 article();
+
+function showdata(data){
+const image = (document.querySelector(".item__img"));
+console.log(image);
+image.innerHTML=`<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+const title = (document.querySelector("#title"));
+console.log(title);
+title.innerHTML = `${data.name}`;
+const price = (document.querySelector("#price"));
+console.log(price);
+price.innerHTML= `${data.price}`;
+const description = (document.querySelector("#description"));
+console.log(description);
+description.innerHTML=`${data.description}`;
+}
+
 
 
 
